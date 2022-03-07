4366d1468cdfbe993a00f0cd46d53e439af01471
Maik Henrique <maik.hsp.contato@gmail.com>
2022-03-07 20:19:24 -0300

    feat: addition of flyway and swagger
     - addition of sample unit test for the dummy endpoint
     - addition of base sql script with the database creation
     - addition of swagger documentation

---
+++ backend/agricultura-familiar/src/main/resources/db/migration/V1.0.0__ddl.sql
@@ -0,0 +1,4 @@
+CREATE DATABASE  IF NOT EXISTS agricultura_familiar_db
+DEFAULT CHARACTER SET utf8
+DEFAULT COLLATE utf8_general_ci;
+USE agricultura_familiar_db;
--- backend/agricultura-familiar/src/test/java/com/es/agriculturafamiliar/AgriculturaFamiliarApplicationTests.java
+++ backend/agricultura-familiar/src/test/java/com/es/agriculturafamiliar/AgriculturaFamiliarApplicationTests.java
@@ -1,13 +1,44 @@
 package com.es.agriculturafamiliar;
 
-import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
+import com.es.agriculturafamiliar.controller.DummyController;
 
-@SpringBootTest
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.RequestBuilder;
+
+import static org.hamcrest.Matchers.empty;
+import static org.hamcrest.Matchers.hasProperty;
+import static org.hamcrest.Matchers.is;
+import static org.hamcrest.Matchers.not;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.anyString;
+import static org.mockito.ArgumentMatchers.eq;
+import static org.mockito.BDDMockito.given;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.model;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.view;
+
+
+// @SpringBootTest(classes = {DummyController.class})
+// @ExtendWith(SpringExtension.class)
+
+@WebMvcTest(DummyController.class)
 class AgriculturaFamiliarApplicationTests {
 
+	@Autowired
+	private MockMvc mockMvc;
+	
 	@Test
-	void contextLoads() {
+	void dummyEndpoint_ShouldReturnMessage_WhenGetIsPerformed() throws Exception {
+		mockMvc.perform(get("/api/dummy")).andExpect(status().isOk());
 	}
 
 }
