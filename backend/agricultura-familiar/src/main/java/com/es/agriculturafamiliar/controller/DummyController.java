45261384db8e1af2499e88239ca331fe3829e6b5
Maik Henrique <maik.hsp.contato@gmail.com>
2022-03-07 19:25:23 -0300

    fix: correction of docker issues and endpoint mapping

--- backend/agricultura-familiar/src/main/java/com/es/agriculturafamiliar/controller/DummyController.java
+++ backend/agricultura-familiar/src/main/java/com/es/agriculturafamiliar/controller/DummyController.java
@@ -2,9 +2,11 @@ package com.es.agriculturafamiliar.controller;
 
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-@RestController("/api/dummy")
+@RestController()
+@RequestMapping("/api/dummy")
 public class DummyController {
     
     @GetMapping
