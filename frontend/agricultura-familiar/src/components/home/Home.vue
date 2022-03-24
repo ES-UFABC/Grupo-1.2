e147ac51c184ca7b938675ad8d0cd7324fd45df0
Matheus Ribeiro <matheus.ribeiro@cotarys.com.br>
2022-03-24 02:12:43 -0300

    feature: Consumidor screen form html markup

---
+++ frontend/agricultura-familiar/src/components/home/Home.vue
@@ -0,0 +1,22 @@
+<template>
+  <div>
+    <p>aaaa</p>
+    <router-view />
+
+  </div>
+</template>
+<script>
+  export default {
+    name: 'Home',
+    created() {
+      console.log(this.$router);
+      //const currentPath = this.$router.history.current.path;
+      //if (window.localStorage.getItem('authenticated') === 'false') {
+      //  this.$router.push('/login');
+      //}
+      //if (currentPath === '/' || currentPath === '/app') {
+      //  this.$router.push('/app/dashboard');
+      //}
+    },
+  };
+</script>
