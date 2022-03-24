b29b22d8c95a4aad993afb2593e29a720d538a35
Matheus Ribeiro <matheus.ribeiro@cotarys.com.br>
2022-03-24 02:12:43 -0300

    feature: Consumidor screen form html markup

--- frontend/agricultura-familiar/src/App.vue
+++ frontend/agricultura-familiar/src/App.vue
@@ -1,60 +1,36 @@
 <template>
-  <div id="app">
-    <img src="./assets/logo.png">
-    <h1>{{ msg }}</h1>
-    <h2>Essential Links</h2>
-    <ul>
-      <li><a href="https://vuejs.org" target="_blank">Core Docs</a></li>
-      <li><a href="https://forum.vuejs.org" target="_blank">Forum</a></li>
-      <li><a href="https://chat.vuejs.org" target="_blank">Community Chat</a></li>
-      <li><a href="https://twitter.com/vuejs" target="_blank">Twitter</a></li>
-    </ul>
-    <h2>Ecosystem</h2>
-    <ul>
-      <li><a href="http://router.vuejs.org/" target="_blank">vue-router</a></li>
-      <li><a href="http://vuex.vuejs.org/" target="_blank">vuex</a></li>
-      <li><a href="http://vue-loader.vuejs.org/" target="_blank">vue-loader</a></li>
-      <li><a href="https://github.com/vuejs/awesome-vue" target="_blank">awesome-vue</a></li>
-    </ul>
+  <div :class="{root: true, sidebarClose: false}">
+    <!--<Helper />
+  <Header />
+  <Sidebar />-->
+    <div>
+
+    </div>
+    <b-container ref="content">
+      <transition name="router-animation">
+        <router-view />
+      </transition>
+    </b-container>
+    <footer class="contentFooter">
+      Muda
+    </footer>
   </div>
 </template>
 
 <script>
-export default {
-  name: 'app',
-  data () {
-    return {
-      msg: 'Welcome to Your Vue.js App'
-    }
-  }
-}
+  export default {
+    name: 'App',
+    created() {
+      //this.$router.push('/home');
+      //const currentPath = this.$router.history.current.path;
+      //if (window.localStorage.getItem('authenticated') === 'false') {
+      //  this.$router.push('/login');
+      //}
+      //if (currentPath === '/' || currentPath === '/app') {
+      //  this.$router.push('/app/dashboard');
+      //}
+    },
+  };
 </script>
 
-<style lang="scss">
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-
-h1, h2 {
-  font-weight: normal;
-}
-
-ul {
-  list-style-type: none;
-  padding: 0;
-}
-
-li {
-  display: inline-block;
-  margin: 0 10px;
-}
-
-a {
-  color: #42b983;
-}
-</style>
+<!--<style src="./styles/theme.scss" lang="scss" />-->
--- frontend/agricultura-familiar/src/main.js
+++ frontend/agricultura-familiar/src/main.js
@@ -1,7 +1,22 @@
 import Vue from 'vue'
-import App from './App.vue'
+import { BootstrapVue, IconsPlugin } from 'bootstrap-vue'
+
+import './styles/app.scss';
+
+// Make BootstrapVue available throughout your project
+Vue.use(BootstrapVue)
+// Optionally install the BootstrapVue icon components plugin
+Vue.use(IconsPlugin)
+
+
+
+
+import App from './App'
+import router from './routes';
+
 
 new Vue({
   el: '#app',
+  router,
   render: h => h(App)
 })
