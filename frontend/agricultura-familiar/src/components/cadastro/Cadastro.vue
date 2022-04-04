a2ad74868b4cfce8f84a4fe678f5da426f1959cc
Matheus Ribeiro <matheus.ribeiro@cotarys.com.br>
2022-04-04 11:54:02 -0300

    feature: abstracting register behaviors in a parent component

--- frontend/agricultura-familiar/src/components/cadastro/Cadastro.vue
+++ frontend/agricultura-familiar/src/components/cadastro/Cadastro.vue
@@ -1,55 +1,34 @@
 <script>
-export default {
-  name: 'Cadastro',
-  data () {
-    return {
-      form: {}
-    }
-  },
-
-  methods: {
-    /* Add functions here that will be available in all components that inherit from this one */
-
-    /* Here we can add complicated functions, that do things like parse a url for search params*/
-    parseURLFilters () {
-      const parsedFilters = Object.keys(this.filters).map(filter => {
-        return { [filter]: `filters[${filter}]` }
-      })
-      const url = new URLSearchParams(window.location.search)
-
-      parsedFilters.map(filterObj => {
-        let dataProp = Object.keys(filterObj)[0]
-        let filterName = Object.values(filterObj)[0]
-         return this[dataProp] = url.get(filterName)
-      })
+  export default {
+    name: 'Cadastro',
+    data () {
+      return {
+        form: {},
+        endereco: {
+          CEP: '',
+          numero: '',
+          complemento: '',
+          rua: '',
+          bairro: '',
+          municipio: ''
+        }
+      }
     },
 
-    submitSearch (searchUrl, filters = {}) {
-      this.$http.get(searchUrl, {
-        params: { query: this.searchText, filters }
-      }).then(response => {
-        /* emit event to handle results in extended components */
-        this.$emit('setSearchResults', {searchResults: response.body})
-      }, error => {
-        /* handle errors */
-      })
-    },
+    methods: {
+      enviar(event) {
+        event.preventDefault()
+        let uri = `${process.env.SERVER_URI}/${this.endpoint}`
+        console.log(uri)
 
-    async obterEnderecoPorCEP(cep) {
-      let apiUri = process.env.VIACEP_API_URI?.replace('{CEP}', cep)
-      await this.$http.get(apiUri)
-        .then((response) => {
-          //this.form.complemento = response.data.complemento;
-          //this.form.rua = response.data.logradouro;
-          //this.form.bairro = response.data.bairro;
-          //this.form.municipio = response.data.localidade;
-          console.log(response.data);
-          return response.data;
-        })
-        .error((error) => {
-          console.log(error);
-        })
+        this.$http.post(uri, this.model)
+          .then((response) => {
+            this.exibirRespostaDaSubmissao(response);
+          })
+          .catch((error) => {
+            console.log(error);
+          });
+      }
     }
   }
-}
 </script>
---
+++ frontend/agricultura-familiar/src/components/endereco/Endereco.vue
@@ -0,0 +1,151 @@
+<template>
+  <div class="my-lg-5">
+    <h3>{{ titulo }}</h3>
+    <hr />
+
+    <!-- cep e Cidade -->
+    <b-form-row>
+      <b-col>
+        <b-form-group id="input-group-txt-cep"
+                      label="CEP"
+                      label-for="txt-cep">
+          <b-form-input id="txt-cep"
+                        v-model="endereco.cep"
+                        v-mask="'#####-###'"
+                        type="text"
+                        placeholder=""
+                        @change="carregarEndereco"
+                        required></b-form-input>
+
+        </b-form-group>
+      </b-col>
+      <b-col>
+        <b-form-group id="input-group-txt-municipio"
+                      label="Municipio"
+                      label-for="txt-municipio">
+          <b-form-input id="txt-municipio"
+                        v-model="endereco.municipio"
+                        type="text"
+                        @change="salvarEndereco"
+                        placeholder=""
+                        required></b-form-input>
+
+        </b-form-group>
+      </b-col>
+    </b-form-row>
+
+    <!-- Endereço -->
+    <b-form-row>
+      <b-col>
+        <b-form-group id="input-group-txt-endereco"
+                      label="Endereço"
+                      label-for="txt-endereco">
+          <b-input-group>
+            <b-input-group-prepend>
+              <b-input-group-text>
+                <b-icon icon="x" />
+              </b-input-group-text>
+            </b-input-group-prepend>
+            <b-form-input id="txt-endereco"
+                          v-model="endereco.rua"
+                          type="text"
+                          @change="salvarEndereco"
+                          placeholder=""></b-form-input>
+          </b-input-group>
+
+        </b-form-group>
+      </b-col>
+    </b-form-row>
+
+
+    <!-- Numero, Bairro e Complemento -->
+    <b-form-row>
+      <b-col>
+        <b-form-group id="input-group-txt-numero"
+                      label="Número"
+                      label-for="txt-numero">
+
+          <b-form-input id="txt-numero"
+                        v-model="endereco.numero"
+                        type="text"
+                        @change="salvarEndereco"
+                        placeholder=""></b-form-input>
+
+        </b-form-group>
+      </b-col>
+      <b-col>
+        <b-form-group id="input-group-txt-bairro"
+                      label="Bairro"
+                      label-for="txt-bairro">
+          <b-form-input id="txt-bairro"
+                        v-model="endereco.bairro"
+                        type="text"
+                        @change="salvarEndereco"
+                        placeholder=""></b-form-input>
+
+        </b-form-group>
+      </b-col>
+      <b-col>
+        <b-form-group id="input-group-txt-complemento"
+                      label="Complemento"
+                      label-for="txt-complemento">
+
+          <b-form-input id="txt-complemento"
+                        v-model="endereco.complemento"
+                        type="text"
+                        @change="salvarEndereco"
+                        placeholder=""></b-form-input>
+
+        </b-form-group>
+      </b-col>
+    </b-form-row>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: 'Endereco',
+    props: {
+      titulo: {
+        type: String,
+        required: true
+      }
+    },
+    data() {
+      return {
+        endereco: {
+          id: 0,
+          cep: '',
+          numero: '',
+          complemento: '',
+          rua: '',
+          bairro: '',
+          municipio: ''
+        }
+      }
+    },
+
+    methods: {
+      carregarEndereco() {
+        let apiUri = process.env.VIACEP_API_URI.replace('{CEP}', this.endereco.cep || '')
+
+        this.$http.get(apiUri)
+          .then((response) => {
+            this.endereco.complemento = response.data.complemento;
+            this.endereco.rua = response.data.logradouro;
+            this.endereco.bairro = response.data.bairro;
+            this.endereco.municipio = response.data.localidade;
+            //TODO: colocar foco no input referente ao número
+            console.log(this.numero.$el);
+            this.salvarEndereco();
+          })
+          .catch((error) => {
+            console.log(error);
+          })
+      },
+      salvarEndereco() {
+        this.$emit('salvar-endereco', this.endereco)
+      }
+    }
+  }
+</script>
