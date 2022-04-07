60cadf5a1e0cce99ead36e0cdcf4fa3fc2c5c569
Matheus Ribeiro <matheus.ribeiro@cotarys.com.br>
2022-04-07 15:35:49 -0300

    feature: application main route component

--- frontend/agricultura-familiar/src/views/root/Root.vue
+++ frontend/agricultura-familiar/src/views/root/Root.vue
@@ -1,5 +1,14 @@
 <template>
-  <p>Muda Root</p>
+  <div>
+    <b-card title="Muda" sub-title="___">
+      <b-card-text>
+        Como você vai fazer parte da nossa comunidade?
+      </b-card-text>
+
+      <router-link to="/consumidor" class="card-link">Quero Comprar!</router-link>
+      <router-link to="/produtor" class="card-link">Sou Produtor!</router-link>
+    </b-card>
+  </div>
 </template>
 
 <script>
