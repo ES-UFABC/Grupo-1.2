c041d92f8281846f0585654aca85de44a709ee4a
FusiDaniel <fpires.daniel@gmail.com>
2022-04-11 18:26:58 -0300

    Added all navbar elements and first products grid

--- frontend/agricultura-familiar/src/components/navbar/NavBar.vue
+++ frontend/agricultura-familiar/src/components/navbar/NavBar.vue
@@ -1,8 +1,9 @@
 <template>
   <div>
-    <b-navbar toggleable="lg" type="dark" variant="info">
+    <b-navbar toggleable="lg">
       <b-navbar-brand href="/">
-        <b-img src="../../../public/muda_green_small.png" alt="Muda"></b-img> Muda
+        <b-img src="../../../public/muda_green_small.png" alt="Muda"></b-img>
+        Muda
       </b-navbar-brand>
 
       <b-navbar-toggle target="nav-collapse"></b-navbar-toggle>
@@ -10,12 +11,12 @@
       <b-collapse id="nav-collapse" is-nav>
         <b-navbar-nav>
           <b-nav-item href="#">Orgânicos</b-nav-item>
-          <b-nav-item href="#">Estação</b-nav-item>
           <b-nav-item href="#">Exóticos</b-nav-item>
           <b-nav-item href="#">Frutas</b-nav-item>
           <b-nav-item href="#">Legumes</b-nav-item>
+          <b-nav-item href="#">Verduras</b-nav-item>
         </b-navbar-nav>
-        
+
         <b-input-group>
           <b-input-group-prepend>
             <span class="input-group-text">
@@ -23,37 +24,53 @@
             </span>
           </b-input-group-prepend>
 
-          <b-form-input class="SearchInput" size="lg" placeholder="Busque por item ou produtor">
+          <b-form-input
+            class="SearchInput"
+            size="lg"
+            placeholder="Busque por item ou produtor"
+          >
           </b-form-input>
 
           <b-input-group-append>
-             <span class="input-group-text">
-              <b-img src="../../../src/assets/location.svg" alt="Muda"></b-img>
+            <span class="input-group-text">
+              <b-img
+                src="../../../src/assets/location.svg"
+                alt="Location"
+              ></b-img>
             </span>
           </b-input-group-append>
         </b-input-group>
 
-
-        <!-- <b-form-input
-          size="sm"
-          class="mr-sm-2"
-          placeholder="Search"
-        ></b-form-input>
-        <b-button size="sm" class="my-2 my-sm-0" type="submit"
-          >Search</b-button
-        > -->
-
         <!-- Right aligned nav items -->
+
+        <b-container
+          class="bv-example-row"
+          style="background-color: #38C961; max-width: 160px; height: 40px; border-radius: 20px"
+        >
+          <b-row class="text-center">
+            <b-col cols="3">0</b-col>
+            <b-col cols="3"
+              ><b-img
+                src="../../../src/assets/shopping-cart.svg"
+                alt="Shopping cart"
+              ></b-img
+            ></b-col>
+            <b-col>R$0,00</b-col>
+          </b-row>
+        </b-container>
+
         <b-navbar-nav class="ml-auto">
-          
-          
-          <b-nav-item-dropdown  no-caret right>
+          <b-nav-item-dropdown no-caret right>
             <!-- Using 'button-content' slot -->
             <template #button-content>
-              <em><img src="../../assets/user.svg" alt="Muda"></em>
+              <em><img src="../../assets/user.svg" alt="User" /></em>
             </template>
-            <b-dropdown-item href="#">Profile</b-dropdown-item>
-            <b-dropdown-item href="#">Sign Out</b-dropdown-item>
+            <b-dropdown-item href="#">Minha conta</b-dropdown-item>
+            <b-dropdown-item href="#">Pedidos</b-dropdown-item>
+            <b-dropdown-item href="#">Chat</b-dropdown-item>
+            <b-dropdown-item href="#">Carteira</b-dropdown-item>
+            <hr>
+            <b-dropdown-item href="#">Sair</b-dropdown-item>
           </b-nav-item-dropdown>
         </b-navbar-nav>
       </b-collapse>
@@ -62,22 +79,33 @@
 </template>
 
 <style scoped>
-  .input-group-text {
-    width: 50px;
-    border: none;
-    background-color: #ffffff;
-    display: flex;
-    align-items: center;
-    justify-content: center;
-  }
-  .input-group-text img {
-    height: 26px;
-  }
-  .SearchInput {
-    border: none;
-  }
-  .input-group {
-    margin-left: 1rem;
-    margin-right: 1rem;
-  }
+.input-group-text {
+  width: 50px;
+  border: none;
+  background-color: #f8f8f8;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+}
+.SearchInput {
+  background-color: #f8f8f8;
+}
+.input-group-text img {
+  height: 26px;
+}
+.SearchInput {
+  border: none;
+}
+.input-group {
+  margin-left: 1rem;
+  margin-right: 1rem;
+}
+.navbar {
+  background-color: #ffffff;
+}
+.shooping-cart {
+  height: 16px;
+}
+.button {
+}
 </style>
---
+++ frontend/agricultura-familiar/src/components/productcard/ProductCard.vue
@@ -0,0 +1,21 @@
+<template>
+  <b-card
+    title="Nome do Produto"
+    img-top
+    style="margin:8px; max-width:20rem"
+  > 
+    <img v-bind:src=image alt="image" style="width:100%">
+    <b-card-text>
+      ({{ index }}) Pequena descrição do produto.
+    </b-card-text>
+    <template #footer>
+      <small class="text-muted">Nome do Produtor</small>
+    </template>
+  </b-card>
+</template>
+
+<script>
+export default {
+  props: ["index", "image"],
+};
+</script>
