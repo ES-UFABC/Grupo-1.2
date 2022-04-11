3fd3df6566aa66c1ae4c856d913af2805eaf1a37
Matheus Ribeiro <matheus.ribeiro@cotarys.com.br>
2022-04-11 12:39:27 -0300

    refactor: including UF field in address registration

--- frontend/agricultura-familiar/src/models/endereco.js
+++ frontend/agricultura-familiar/src/models/endereco.js
@@ -1,10 +1,12 @@
 export default class Endereco {
-  constructor(rua, numero, bairro, municipio, cep, complemento) {
-    this.cep: cep;
-    this.numero: numero;
-    this.complemento: complemento;
-    this.rua: rua;
-    this.bairro: bairro;
-    this.municipio: municipio;
+  constructor(rua, numero, bairro, municipio, estado, cep, complemento) {
+    this.cep = cep;
+    this.numero = numero;
+    this.complemento = complemento;
+    this.rua = rua;
+    this.bairro = bairro;
+    this.municipio = municipio;
+    this.estado = estado;
+    this.estados = []
   }
 }
