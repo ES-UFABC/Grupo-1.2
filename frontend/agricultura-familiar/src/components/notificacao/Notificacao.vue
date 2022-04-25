582737fc74a42faac5ff966fad3d22bc09d58f9b
Matheus Ribeiro <matheus.ribeiro@cotarys.com.br>
2022-04-25 14:27:45 -0300

    fix: 'NaN dias atrás' notification text

--- frontend/agricultura-familiar/src/components/notificacao/Notificacao.vue
+++ frontend/agricultura-familiar/src/components/notificacao/Notificacao.vue
@@ -43,25 +43,25 @@
         var interval = seconds / 31536000;
 
         if (interval > 1)
-          return `${Math.floor(interval)} anos`;
+          return `${Math.floor(interval)} anos atrás`;
         
         interval = seconds / 2592000;
         if (interval > 1)
-          return `${Math.floor(interval)} meses`;
+          return `${Math.floor(interval)} meses atrás`;
 
         interval = seconds / 86400;
         if (interval > 1)
-          return `${Math.floor(interval)} dias`;
+          return `${Math.floor(interval)} dias atrás`;
         
         interval = seconds / 3600;
         if (interval > 1) 
-          return `${Math.floor(interval)} horas`;
+          return `${Math.floor(interval)} horas atrás`;
         
         interval = seconds / 60;
         if (interval > 1) 
-          return `${Math.floor(interval)} minutos`;
+          return `${Math.floor(interval)} minutos atrás`;
         
-        return `${Math.floor(seconds)} segundos`;
+        return `${Math.floor(seconds)} segundos atrás`;
       }
     }
   }
