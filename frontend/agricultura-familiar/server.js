8b87b081299a9954e7248f70d0081a90a200816d
Maik Henrique <maik.hsp.contato@gmail.com>
2022-05-04 20:47:59 -0300

    fix: changing build strategy to use vue-cli instead of webpack

--- frontend/agricultura-familiar/server.js
+++ frontend/agricultura-familiar/server.js
@@ -7,7 +7,7 @@ const app = express()
 app.use('/', serveStatic(path.join(__dirname, '/dist')))
 
 app.get(/.*/, function (req, res) {
-    res.sendFile(path.join(__dirname, '/dist/build.js'))
+    res.sendFile(path.join(__dirname, '/dist/index.html'))
 })
 
 const port = process.env.PORT || 9000
